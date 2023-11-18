0cee3ee709afc4bb461abc936a41dd9bdde8b8b4
Tiffany Torres <torresrtiffany@gmail.com>
2023-11-18 03:15:20 -0800

    add different functions

--- src/App.js
+++ src/App.js
@@ -10,8 +10,83 @@ axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
 
+const consumer= axios.create({
+  baseURL: "http://127.0.0.1:8000"
+});
+
 function App() {
- 
+  const[currentUser, setCurrentUser] = useState();
+  const[signupToggle, setSignupToggle] = useState(false);
+  const[email, setEmail] = useState(" ");
+  const[username, setUsername] = useState(" ")
+  const[password, setPassword] = useState(" ")
+
+  useEffect(()=>{
+    M.AutoInit();
+    consumer.get("/customer/user")
+    .then(function(res){
+      setCurrentUser(true);
+    })
+    .catch(function(error){
+      setCurrentUser(false)
+    });
+  },[])
+
+  function update_form_btn(){
+    if(signupToggle){
+      document.getElementById("form_btn").innerHTML = "Signup";
+      setSignupToggle(false);
+
+    } else {
+      document.getElementById("form_btn").innerHTML = "Login";
+      setSignupToggle(true);
+    }
+
+  }
+
+  function submitSignup(e){
+    e.preventDefault();
+    consumer.post(
+      "/customer/signup",
+      {
+        email: email,
+        username: username,
+        password: password,
+      }
+    ).then(function(res){
+      consumer.post(
+        "/customer/login",
+        {
+          email: email,
+          password: password
+        }
+      ).then(function(res){
+        setCurrentUser(true);
+      });
+    });
+
+  }
+ if (currentUser){
+  return(
+    <div>
+      <nav>
+        <div className='nav-wrapper'>
+          <a href="#" className='brand-logo'>Bearded Garage Mobile Services</a>
+          <ul className='right hide-on-med-and-down'>
+            <li>
+              <form onSubmit={e => submitLogout(e)}>
+                <button type="submit" className='waves-effect waves-light btn'>LOG OUT</button>
+              </form>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className='center'>
+        <h2>Logged IN!</h2>
+      </div>
+    </div>
+  );
+ }
   return (
     <div className='App'>
       <nav>
@@ -43,7 +118,7 @@ function App() {
           </li>
         ) : (
           <li>
-            <button id="form_btn" onClick={updateFormBtn} className="btn waves-effect waves-light">Register</button>
+            <button id="form_btn" onClick={updateFormBtn} className="btn waves-effect waves-light">Signup</button>
           </li>
         )}
       </ul>
